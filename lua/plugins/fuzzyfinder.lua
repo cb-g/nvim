a10810c4a80281327fb66cb1562d996ef8b1818e
cb-g <55954688+cb-g@users.noreply.github.com>
2024-05-22 02:25:49 +0200

    telescope keymaps

--- lua/plugins/fuzzyfinder.lua
+++ lua/plugins/fuzzyfinder.lua
@@ -5,5 +5,13 @@ return {
   dependencies = { 
     'nvim-lua/plenary.nvim' 
   },
+  config = function()
+    require('telescope').setup({
+      vim.keymap.set("n", "<leader>fs", ":Telescope find_files<cr>"),
+      vim.keymap.set("n", "<leader>fv", ":Telescope git_files<cr>"),
+      vim.keymap.set("n", "<leader>fg", ":Telescope live_grep<cr>"),
+      vim.keymap.set("n", "<leader>fo", ":Telescope oldfiles<cr>"),
+    })
+  end,
 
 }
