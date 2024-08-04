e558a896a5feddfdbb597dd5841b1b676e8d2218
cb-g <orbiter-caster0p@icloud.com>
2024-08-04 13:57:42 +0200

    rainbow_csv

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -219,4 +219,24 @@ return {
     lazy = false,
   },
 
+  {
+    'cameron-wags/rainbow_csv.nvim',
+    config = true,
+    ft = {
+      'csv',
+      'tsv',
+      'csv_semicolon',
+      'csv_whitespace',
+      'csv_pipe',
+      'rfc_csv',
+      'rfc_semicolon'
+    },
+    cmd = {
+      'RainbowDelim',
+      'RainbowDelimSimple',
+      'RainbowDelimQuoted',
+      'RainbowMultiDelim'
+    }
+  },
+
 }
